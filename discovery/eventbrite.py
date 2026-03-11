2693982b9034f8e7090384888bd34d0a9437667a
ux-agent <ux@example.com>
2026-03-11 23:37:53 +0530

    changes made in eventbrite.py

--- discovery/eventbrite.py
+++ discovery/eventbrite.py
@@ -1,21 +1,30 @@
+from wsgiref import headers
+
 import requests
 from bs4 import BeautifulSoup
 
 
-SEARCH_URL = "https://www.eventbrite.com/d/online/ux-conference/"
+SEARCH_URL = "https://www.eventbrite.com/d/online/ux/"
 
 
 def discover_eventbrite_events():
     events = []
 
     headers = {
-        "User-Agent": "Mozilla/5.0"
+       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept-Language": "en-US,en;q=0.9",
     }
+
+    response = requests.get(
+    SEARCH_URL,
+    headers=headers,
+    timeout=15
+    )
 
     response = requests.get(SEARCH_URL, headers=headers, timeout=10)
 
     if response.status_code != 200:
-        print("Failed to fetch Eventbrite page")
+        print("Failed to fetch Eventbrite page", response.status_code)
         return events
 
     soup = BeautifulSoup(response.text, "html.parser")
